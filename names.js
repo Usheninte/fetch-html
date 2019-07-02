38444d6812d9762034b00ccf1d9b4099f181f80b
Usheninte <jrandangana@gmail.com>
2019-07-02 19:10:44 +0100

    added js

--- names.js
+++ names.js
@@ -1,6 +1,10 @@
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(function (response) {
     // The JSON data will arrive here
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
   })
   .catch(function (err) {
     // If an error occured, you will catch it here
