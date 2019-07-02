1f0ca0c44f67201767fabdeb0fbe207ce7c57ef6
Usheninte <jrandangana@gmail.com>
2019-07-02 19:27:40 +0100

    building js

--- names.js
+++ names.js
@@ -11,13 +11,13 @@ div.innerHTML = 'Name: ' + data[i].name;
 mainContainer.appendChild(div);
 
 fetch('https://jsonplaceholder.typicode.com/users')
-  .then(function (response) {
-    // The JSON data will arrive here
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    // If an error occured, you will catch it here
-  })
+    .then(function (response) {
+      // The JSON data will arrive here
+      return response.json();
+    })
+    .then(function (data) {
+      appendData(data);
+    })
+    .catch(function (err) {
+      // If an error occured, you will catch it here
+    })
