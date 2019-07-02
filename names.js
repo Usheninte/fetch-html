1a76d1e5ca230fbdbc2d13a3123ad3cd466fa615
Usheninte <jrandangana@gmail.com>
2019-07-02 19:29:57 +0100

    building js

--- names.js
+++ names.js
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-mainContainer.appendChild(div);
-
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(function (response) {
       // The JSON data will arrive here
@@ -26,6 +16,7 @@ function appendData(data) {
   for (var i = 0; i < data.length; i++) {
     // append each person to page
     var div = document.createElement("div");
-    div.innerHTML = 'Name: ' + data[i].name;
+    div.innerHTML = data[i].name;
+    mainContainer.appendChild(div);
   }
 }
