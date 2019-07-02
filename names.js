dc23f9b10e52840838c5417b139ae684f1df5370
Usheninte <jrandangana@gmail.com>
2019-07-02 19:07:36 +0100

    added js

---
+++ names.js
@@ -0,0 +1,7 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(function (response) {
+    // The JSON data will arrive here
+  })
+  .catch(function (err) {
+    // If an error occured, you will catch it here
+  })
